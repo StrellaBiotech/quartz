b5fa1b589c2df91bde29be4fbeb5e013d6553635
Carson Boden <carson@strellabiotech.com>
2026-02-05 17:20:53 -0800

    update: Use `docs.strella.ai` as baseUrl.

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "docs.strella.ai",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
